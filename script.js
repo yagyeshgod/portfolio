eeadff5673b82f361d628e292630546273ec5daa
yagyeshgod <yagyesh.godiyal@gmail.com>
2025-10-24 12:30:21 +0530

    Initial portfolio setup

---
+++ script.js
@@ -0,0 +1,10 @@
+const sections = document.querySelectorAll('section');
+
+window.addEventListener('scroll', () => {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      section.classList.add('show');
+    }
+  });
+});
